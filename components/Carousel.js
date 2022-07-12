803622d922d1e5830934534fde877cea145747b3
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2022-07-12 05:15:05 +0100

    image sizing

--- components/Carousel.js
+++ components/Carousel.js
@@ -43,6 +43,9 @@ const Carousel = () => {
               className="d-block"
               priority={true}
               placeholder="blur"
+              width={1350}
+              height={600}
+
               // objectFit="cover"
             />
           </div>
@@ -53,6 +56,9 @@ const Carousel = () => {
               className="d-block w-100"
               priority={true}
               placeholder="blur"
+              width={1350}
+              height={600}
+
               // objectFit="cover"
             />
             {/*<img src="..." className="d-block w-100" alt="...">*/}
@@ -64,6 +70,8 @@ const Carousel = () => {
               className="d-block w-100"
               priority={true}
               placeholder="blur"
+              width={1350}
+              height={600}
               // objectFit="cover"
             />
             {/*<img src="..." className="d-block w-100" alt="...">*/}
