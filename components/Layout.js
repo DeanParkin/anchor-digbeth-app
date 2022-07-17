1af035f40c951834bc9bf36c77dbedfa1e0e40fe
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2022-07-17 20:13:43 +0100

    favicon update

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,31 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
 import NewsLetter from "./NewsLetter";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Nav />
 
       <div className="">{children}</div>
