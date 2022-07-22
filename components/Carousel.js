2d32e6bb4e8f2cdf0dbbba50d9593c67eca6d25c
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2022-07-22 17:32:48 +0100

    Image update

--- components/Carousel.js
+++ components/Carousel.js
@@ -72,8 +72,8 @@ const Carousel = () => {
               placeholder="blur"
               // width={1350}
               // height={600}
-              objectFit="fill"
-              //objectFit="cover"
+              //objectFit="fill"
+              objectFit="cover"
             />
           </div>
           <div className="carousel-item">
@@ -83,8 +83,8 @@ const Carousel = () => {
               className="d-block w-100"
               priority={true}
               //placeholder="blur"
-              objectFit="fill"
-              //objectFit="cover"
+              //objectFit="fill"
+              objectFit="cover"
 
               // objectFit="cover"
             />
@@ -97,9 +97,8 @@ const Carousel = () => {
               className="d-block w-100"
               priority={true}
               //placeholder="blur"
-              objectFit="fill"
-              //objectFit="cover"
-              // objectFit="cover"
+              //objectFit="fill"
+              objectFit="cover"
             />
             {/*<img src="..." className="d-block w-100" alt="...">*/}
           </div>
