6b7a8287bc7393435d32e98b4f9b37b4095964b7
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2022-11-09 17:34:41 +0000

    email update

--- pages/api/contactsend.js
+++ pages/api/contactsend.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
   try {
     const emailRes = await transporter.sendMail({
       from: `${contactName} <${contactEmail}>`,
-      to: "deanparkin@live.co.uk",
+      to: "julian@theanchordigbeth.com",
       subject: `Contact Form Submission from ${contactName}`,
       html: `<p>You have a new contact form submission</p><br>
       <p><strong>Name: </strong> ${contactName}</p><br>
