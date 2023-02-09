7957b4d88d44b6ffe9b460480e1556ce930e15e2
Dean Parkin <Deanparkin@live.co.uk>
2023-02-09 08:07:26 +0000

    update and hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -108,6 +108,17 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* <PageLoading /> */}
+      {/* <!-- Hotjar Tracking Code for https://www.theanchordigbeth.com/ --> */}
+<Script id="hotjar">
+    {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3348840,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+</Script>
       <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-H9Q4C999HC"
